dafe7a0421b85c92f3df5e13803b034d7cca530e
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2022-03-10 01:46:10 +0700

    final velocity added & type from const removed

--- physics-formulas/kinematics/calculate_velocity.zig
+++ physics-formulas/kinematics/calculate_velocity.zig
@@ -9,22 +9,22 @@ pub fn print(number: f64) void {
 
 // constant acceleration
 pub fn calculate_velocity(displacement: f64, time: f64) f64 {
-  const v: f64 = displacement / time;
+  const v = displacement / time;
   return v;
 }
 
 pub fn calculate_displacement(x0: f64, x1: f64) f64 {
-  const d: f64 = x1 - x0;
+  const d = x1 - x0;
   return d;
 }
 
 pub fn calculate_time(displacement: f64, velocity: f64) f64 {
-  const t: f64 = displacement / velocity;
+  const t = displacement / velocity;
   return t;
 }
 
 pub fn calculate_final_position(x0: f64, velocity: f64, time: f64) f64 {
-  const x1: f64 = x0 + velocity * time;
+  const x1 = x0 + velocity * time;
   return x1;
 }
 
@@ -33,6 +33,11 @@ pub fn calculate_acceleration(v0: f64, v1: f64, time: f64) f64 {
   return a;
 }
 
+pub fn calculate_final_velocity(v0: f64, acceleration: f64, time: f64) f64 {
+  const v1 = v0 + acceleration * time;
+  return v1;
+}
+
 pub fn main() void {
-  print(calculate_final_position(0, 10, 5));
+  print(calculate_final_velocity(0, 10, 5));
 }
